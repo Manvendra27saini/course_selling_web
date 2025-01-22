e9e28b6ea86d12d61c6862e4b4a5f3d5a6451481
Manvendra27saini <manvendrasaini2005@gmail.com>
2025-01-22 20:28:52 +0530

    defined database schema

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const mongoose=require('mongoose');
 const {userRouter} = require('./routes/user');
 const { courseRouter} = require('./routes/course');
 const  {adminRouter} = require('./routes/admin');
+
 const app= express();
+
 app.use("/api/v1/user" ,userRouter);
 app.use("/api/v1/course" ,courseRouter);
 app.use("/api/v1/admin" ,adminRouter);
